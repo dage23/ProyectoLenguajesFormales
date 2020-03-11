668feb85e266ad51af2e9597c22c1a98ce487146
Allan Davila <allan.davila@hotmail.com>
2020-03-10 19:13:12 -0600

    Creado nodo arbol

---
+++ ProyectoLenguajesFormales/NodoArbol.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoLenguajesFormales
+{
+    class NodoArbol
+    {
+        public string first;
+        public string last;
+        public NodoArbol hijoIzq;
+        public NodoArbol hijoDer;
+        public int id;
+    }
+}
